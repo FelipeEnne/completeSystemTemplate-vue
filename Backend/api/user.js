9430b702bdb116a31498eaceece8b7a5d6f588fc
Felipe Enne <felipeenne@gmail.com>
2022-01-29 17:30:15 -0300

    add more verification auths

--- Backend/api/user.js
+++ Backend/api/user.js
@@ -13,6 +13,9 @@ module.exports = app => {
         const user = {...req.body};
         if(req.params.id) user.id = req.params.id;
 
+        if(!req.originalUrl.startsWith('/users')) user.admin = false;
+        if(!req.user || !req.user.admin) user.admin = false;
+
         try {
             existsOrError(user.name, 'The name is missing');
             existsOrError(user.email,'The email is missing');
@@ -35,6 +38,7 @@ module.exports = app => {
             app.db('users')
                 .update(user)
                 .where({id: user.id})
+                .whereNull('deletedAt')
                 .then(_ => res.status(204).send())
                 .catch(err => res.status(500).send(err))
         } else {
@@ -48,6 +52,7 @@ module.exports = app => {
     const get = (req, res) => {
         app.db('users')
             .select('id', 'name', 'email', 'admin')
+            .whereNull('deletedAt')
             .then(users => res.json(users))
             .catch(err => res.status(500).send(err))
     }
@@ -56,10 +61,28 @@ module.exports = app => {
         app.db('users')
             .select('id', 'name', 'email', 'admin')
             .where({id: req.params.id})
+            .whereNull('deletedAt')
             .first()
             .then(user => res.json(user))
             .catch(err => res.status(500).send(err))
     }
+    
+    const remove = async (req, res) => {
+        try {
+            const articles = await app.db('articles')
+                .where({ userId: req.params.id })
+            notExistsOrError(articles, 'User have articles')
 
-    return { save, get, getById }
+            const rowsUpdated = await app.db('users')
+                .update({deleteAt: new Date()})
+                .where({ id: req.params.id })
+            existsOrError(rowsUpdated, 'User not found')
+
+            res.status(204).send()
+        } catch(msg) {
+            res.status(400).send(msg)
+        }
+    }
+
+    return { save, get, getById, remove }
 }
---
+++ Backend/migrations/20220129200309_add_deleted_at_table_users.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('users', table => {
+      table.timestamp('deleteAt')
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.alterTable('users', table => {
+        table.dropColumn('deletedAt')
+    })
+};
