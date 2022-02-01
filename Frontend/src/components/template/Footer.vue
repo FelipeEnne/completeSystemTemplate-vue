4f43c452fa9b1e1a2489c04b1830697e81c52834
Felipe Enne <felipeenne@gmail.com>
2022-01-31 22:27:06 -0300

    start frontend

---
+++ Frontend/src/components/template/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer class="footer">
+        <span>Made by Felipe Enne Mendes Ribeiro</span>
+    </footer>
+</template>
+
+<script>
+export default ({
+    name: 'Footer'
+})
+</script>
+
+
+<style>
+    .footer {
+        grid-area: footer;
+        background-color: #DDD;
+        color: #333;
+
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding-right: 10px;
+    }
+</style>
