30aae7d5a62a27ccb4cbe835d7bdecdf4c746caa
Felipe Enne <felipeenne@gmail.com>
2022-02-08 17:35:21 -0300

    add stat

---
+++ Frontend/src/components/home/Stat.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="stat">
+        <div class="stat-icon">
+            <i :class="icon" :style="style"></i>
+        </div>
+        <div class="stat-info">
+            <span class="stat-title">{{title}}</span>
+            <span class="stat-value">{{value}}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default ({
+   name:'Stat',
+   props: ['title', 'value', 'icon', 'color'],
+   computed: {
+       style() {
+           return "color: "+(this.color || "#000")
+       }
+   }
+})
+</script>
+
+
+<style>
+
+</style>
