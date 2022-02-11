259eb3348d53b57bd3eb692ab3fdfa5f309c5abb
Felipe Enne <felipeenne@gmail.com>
2022-02-11 19:37:51 -0300

    create tabs

--- Frontend/src/components/admin/AdminPages.vue
+++ Frontend/src/components/admin/AdminPages.vue
@@ -2,15 +2,38 @@
     <div class="admin-pages">
        <PageTitle icon="fa fa-cogs" main="Admin"
             sub="Register" />
+        <div class="admin-pages-tabs">
+            <b-card no-body>
+                <b-tabs card>
+                    <b-tab title='Article' active>
+                        <ArticleAdmin />
+                    </b-tab>
+                     <b-tab title='Categories'>
+                        <CategoryAdmin />
+                    </b-tab>
+                     <b-tab title='User'>
+                        <UserAdmin />
+                    </b-tab>
+                </b-tabs>
+            </b-card>
+        </div>
     </div>
 </template>
 
 <script>
 import PageTitle from '../template/PageTitle.vue'
+import ArticleAdmin from './ArticleAdmin.vue'
+import CategoryAdmin from './CategoryAdmin.vue'
+import UserAdmin from './UserAdmin.vue'
 
 export default ({
    name:'AdminPages',
-   components: { PageTitle }
+   components: { 
+       PageTitle,
+       ArticleAdmin,
+       CategoryAdmin,
+       UserAdmin
+    }
 })
 </script>
 
---
+++ Frontend/src/components/admin/UserAdmin.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="user-admin">
+       <h1>Component User</h1>
+    </div>
+</template>
+
+<script>
+export default ({
+   name:'UserAdmin',
+})
+</script>
+
+
+<style>
+
+</style>
