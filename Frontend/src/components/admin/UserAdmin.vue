7376c4063f27a138d8cbbfe4af3be9c3da8451aa
Felipe Enne <felipeenne@gmail.com>
2022-02-16 13:58:37 -0300

    add functions save, remove and reset

--- Frontend/src/components/admin/UserAdmin.vue
+++ Frontend/src/components/admin/UserAdmin.vue
@@ -1,13 +1,66 @@
 <template>
     <div class="user-admin">
-       <h1>Component User</h1>
+
+       <b-form>
+          <input id="user-id" type="hidden" v-model="user.id" />
+          <b-row>
+             <b-col md="6" sm="12">
+                <b-form-group label="Name:" label-form="user-name">
+                  <b-form-input id="user-name" type="text"
+                  v-model="user.name" required
+                  placeholder="User name"/>
+                </b-form-group>
+             </b-col>
+             <b-col md="6" sm="12">
+                <b-form-group label="E-mail:" label-form="user-email">
+                  <b-form-input id="user-email" type="text"
+                  v-model="user.email" required
+                  placeholder="User e-mail"/>
+                </b-form-group>
+             </b-col>
+          </b-row>
+
+          <b-form-checkbox id="user-admin" v-model="user.admin" class="mt-3 mb-3" >
+            Adm
+          </b-form-checkbox>
+           <b-row>
+             <b-col md="6" sm="12">
+                <b-form-group label="Password:" label-form="user-password">
+                  <b-form-input id="user-password" type="password"
+                  v-model="user.password" required
+                  placeholder="User password"/>
+                </b-form-group>
+             </b-col>
+             <b-col md="6" sm="12">
+                <b-form-group label="Confirm password:" label-form="user-confirmPassword">
+                  <b-form-input id="user-confirmPassword" type="password"
+                  v-model="user.confirmPassword" required
+                  placeholder="User confirm password"/>
+                </b-form-group>
+             </b-col>
+          </b-row>
+       </b-form>
+
+       <b-button variant="primary" v-if="mode === 'save'"
+         @click="save">
+          Save
+       </b-button>
+       <b-button variant="danger" v-if="mode === 'remove'"
+         @click="remove">
+          Remove
+       </b-button>
+       <b-button class="ml-2" @click="reset">
+          Reset
+       </b-button>
+
+       <hr>
        <b-table hover striped :items="users" :fields="fields" >
        </b-table>
     </div>
 </template>
 
 <script>
-import { baseApiUrl } from '@/global'
+import { baseApiUrl, showError } from '@/global'
 import axios from 'axios'
 
 export default ({
@@ -15,7 +68,7 @@ export default ({
    data: function() {
       return {
          mode: 'save',
-         user: {},
+         user: {admin: false},
          users: [],
          fields: [
             {key: 'id', label: 'Code', sorteable: true},
@@ -32,8 +85,32 @@ export default ({
          axios.get(url).then(res => {
             this.users = res.data
          })
+      },
+      reset() {
+         this.mode = 'save'
+         this.user = {admin: false}
+         this.loadUsers()
+      },
+      save() {
+         const method = this.user.id ? 'put' : 'post'
+         const id = this.user.id ? `/${this.user.id}` : ''
+         axios[method](`${baseApiUrl}/users${id}`, this.user)
+            .then(() => {
+               this.$toasted.global.defaultSucess()
+               this.reset()
+            })
+            .catch(showError)
+      },
+      remove() {
+         const id = this.user.id
+         axios.delete(`${baseApiUrl}/users/${id}`)
+            .then(() => {
+               this.$toasted.global.defaultSucess()
+               this.reset()
+            })
+            .catch(showError)
       }
-   },
+   },   
    mounted() {
       this.loadUsers()
    }
