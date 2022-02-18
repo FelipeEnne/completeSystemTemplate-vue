b1eac4e4d4a76c3e8ce4fe985487aac9e1d8a81e
Felipe Enne <felipeenne@gmail.com>
2022-02-17 21:01:42 -0300

    article by category

--- Backend/api/article.js
+++ Backend/api/article.js
@@ -80,14 +80,14 @@ module.exports = app => {
         const categories = await app.db.raw(queries.categoryWithChildren, categoryId)
         const ids = categories.rows.map(c => c.id)
 
-        app.db({a: 'article', u: 'users'})
-            .select('a.id', 'a.name', 'a.description', 'a.imageUrl', { author: 'u.name'})
+        app.db({a: 'articles', u: 'users'})
+            .select('a.id', 'a.name', 'a.description', 'a.imageUrl', { author: 'u.name' })
             .limit(limit).offset(page * limit - limit)
             .whereRaw('?? = ??', ['u.id', 'a.userId'])
             .whereIn('categoryId', ids)
             .orderBy('a.id', 'desc')
             .then(articles => res.json(articles))
-            .catch(err => res.status(500).send(arr))
+            .catch(arr => res.status(500).send(arr))
     }
 
 
---
+++ Frontend/src/components/article/ArticlesByCategory.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="articles-by-category">
+        <PageTitle icon="fa fa-folder-o"
+            :main="category.name" sub="Category" />
+
+        <ul>
+            <li v-for="article in articles" :key="article.id">
+                <ArticleItem :article="article" />
+            </li>       
+        </ul>
+        <div class="load-more">
+                <button v-if="loadMore"
+                    class="btn btn-lg btn-outline-primary"
+                    @click="getArticles"
+                >
+                    Load More Articles
+                </button>
+            </div>
+    </div>  
+</template>
+
+<script>
+import { baseApiUrl } from '@/global'
+import axios from 'axios'
+import PageTitle  from '../template/PageTitle.vue'
+import ArticleItem  from './ArticleItem.vue'
+
+export default {
+    name: 'ArticlesByCategory',
+    components: { PageTitle, ArticleItem },
+    data: function() {
+        return {
+            category: {},
+            articles: [],
+            page: 1,
+            loadMore: true
+        }
+    },
+    methods: {
+        getCategory() {
+            const url = `${baseApiUrl}/categories/${this.category.id}`
+            axios(url).then(res => this.category = res.data)
+        },
+        getArticles() {
+            const url = `${baseApiUrl}/categories/${this.category.id}/articles?page=${this.page}`
+            axios(url).then(res => {
+                this.articles = this.articles.concat(res.data)
+                this.page++
+
+                if(res.data.length == 0) this.loadMore = false
+            })
+        }
+    },
+    mounted() {
+        this.category.id = this.$route.params.id
+        this.getCategory()
+        this.getArticles()
+    }
+}
+</script>
+
+
+<style>
+    .articles-by-category ul {
+        list-style-type: none;
+        padding: 0px;
+    }
+
+    .articles-by-category .load-more {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 25px;
+    }
+
+</style>
